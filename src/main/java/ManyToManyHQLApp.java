90c131ac6fff3a4f7a7cca90e4aa887f5786b849
LukaszP12 <lukaszp4@onet.eu>
2020-04-20 19:49:19 +0200

    Another commit + ManyToManyHQLApp

--- src/main/java/ManyToManyHQLApp.java
+++ src/main/java/ManyToManyHQLApp.java
@@ -1,3 +1,4 @@
+import net.bytebuddy.implementation.bytecode.assign.TypeCasting;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -27,12 +28,21 @@ public class ManyToManyHQLApp {
 
         String getCompanyZurich = "select c.name from Property p join p.company c where p.city='Zurich'";
 
+        String getCompany2 = "select c.name from Property p join p.company c join c.companyDetail cd where p.city='Barcelona' and cd.residence='Switzerland' ";
+
+        // Swiss company with property in Zurich
+        String getCompany3 = "select c.name from Property p join p.company c join c.companyDetail cd where p.city='Zurich' and cd.residence='Switzerland' ";
+
         currentSession.getTransaction().begin();
         Query query = currentSession.createQuery(getCompany);
         Query query1 = currentSession.createQuery(getCompanyZurich);
+        Query query2 = currentSession.createQuery(getCompany2);
+        Query query3 = currentSession.createQuery(getCompany3);
 
         List<String> resultList = query.getResultList();
         List<String> resultList1 = query1.getResultList();
+        List<String> resultList2 = query2.getResultList();
+        List<String> resultList3 = query3.getResultList();
 
         currentSession.getTransaction().commit();
 
@@ -44,6 +54,16 @@ public class ManyToManyHQLApp {
         System.out.println("Companies based in Zurich");
         for (String name1 : resultList1){
             System.out.println(name1);
+        }
+
+        System.out.println("Swiss company with property in Barcelona");
+        for (String name2 : resultList2){
+            System.out.println(name2);
+        }
+
+        System.out.println("Swiss company with property in Zurich");
+        for (String name : resultList3){
+            System.out.println(name);
         }
 
         currentSession.close();
